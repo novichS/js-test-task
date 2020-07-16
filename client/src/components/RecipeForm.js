c81550c7dd41fa090e6472f15b7d01bef10eb12e
svasyanovych <svitlana.v92@gmail.com>
2020-07-16 21:47:09 +0300

    added basic template of components

---
+++ client/src/components/RecipeForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const RecipeForm = (props) => {
+    return (
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <div className="card teal darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Form</span>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ client/src/components/RecipeVersion.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const RecipeVersion = (props) => {
+    return (
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <div className="card teal darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Versions</span>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
