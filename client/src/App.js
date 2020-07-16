86ee947d3ae3200c70fe92c81d43c8c867ef36f2
svasyanovych <svitlana.v92@gmail.com>
2020-07-16 21:07:58 +0300

    added basic template of page

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,40 @@ import 'materialize-css';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Start - ok!</h1>
-    </div>
+    <>
+      <div className="row">
+        <div className="col s6 offset-s3">
+          <div className="card teal darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">Form</span>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col s6 offset-s3">
+          <div className="card teal darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">List</span>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col s6 offset-s3">
+          <div className="card teal darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">Versions</span>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
   );
 }
 
